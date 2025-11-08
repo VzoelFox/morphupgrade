a7b227b62a30eec4a922bb71ea8b5574730c919b
VzoelFox <itspizolpoks@gmail.com>
2025-11-09 06:15:45 +0700

    Delete transpiler directory

--- transpiler/contoh/ke_js/hasil_sederhana.js
+++
@@ -1,8 +0,0 @@
-let x = 10;
-let nama = "Dunia";
-let aktif = true;
-let nilai_desimal = 123.45;
-let y = x * (2 + 3);
-console.log("Halo,", nama);
-console.log("Hasil perhitungan y adalah:", y);
-console.log("Status aktif:", aktif);
--- transpiler/contoh/ke_python/hasil_sederhana.py
+++
@@ -1,8 +0,0 @@
-x = 10
-nama = "Dunia"
-aktif = True
-nilai_desimal = 123.45
-y = x * (2 + 3)
-print("Halo,", nama)
-print("Hasil perhitungan y adalah:", y)
-print("Status aktif:", aktif)
